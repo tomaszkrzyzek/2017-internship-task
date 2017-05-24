b5993736d238d5cb6ef9d953ea2e7230be422c26
tomaszkrzyzek <tomasz.krzyzek@gmail.com>
2017-05-24 11:31:26 +0200

    Commit1

---
+++ src/main/java/pl/codewise/internship/Scheduler.java
@@ -0,0 +1,7 @@
+package pl.codewise.internship;
+
+/**
+ * Created by tomaszkrzyzek on 24/05/2017.
+ */
+public class Scheduler {
+}
---
+++ src/main/java/pl/codewise/internship/TimerId.java
@@ -0,0 +1,10 @@
+package pl.codewise.internship;
+
+import java.time.Duration;
+
+/**
+ * Created by tomaszkrzyzek on 24/05/2017.
+ */
+public class TimerId {
+    private String id;
+}
